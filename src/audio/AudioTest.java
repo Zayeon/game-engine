9c7864ada31f7e5ef3adb261f0da347ee082f161
Abhinav Akkena <absman09@gmail.com>
2019-07-24 11:05:22 +0100

    Experimented with distance attenuation models

--- src/audio/AudioTest.java
+++ src/audio/AudioTest.java
@@ -7,6 +7,7 @@ public class AudioTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         AudioMaster.init();
         AudioMaster.setListenerData(0 ,0, 0);
+        //AL10.alDistanceModel(AL10.AL_INVERSE_DISTANCE_CLAMPED);
 
         int buffer = AudioMaster.loadSound("res/sounds/bounce.wav");
         Source source = new Source();
@@ -14,14 +15,16 @@ public class AudioTest {
         source.setLooping(true);
         source.play(buffer);
 
-        float xPos = 8;
-        source.setPosition(xPos, 0, 2);
+        float xPos = 0;
+        source.setPosition(xPos, 0, 0);
 
         char c = ' ';
         while (c != 'q'){
             xPos -= 0.03f;
-            source.setPosition(xPos, 0, 2);
+            source.setPosition(xPos, 0, 0);
+            System.out.println(xPos);
             Thread.sleep(10);
+
         }
         source.delete();
         AudioMaster.cleanUp();
