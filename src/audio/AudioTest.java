58cb2e331ad65c89ae719b97e9221b1b1e44d153
Abhinav Akkena <absman09@gmail.com>
2019-07-24 08:38:14 +0100

    Can now play sound effects!

---
+++ src/audio/AudioTest.java
@@ -0,0 +1,25 @@
+package audio;
+
+
+import java.io.IOException;
+
+public class AudioTest {
+    public static void main(String[] args) throws IOException {
+        AudioMaster.init();
+        AudioMaster.setListenerData();
+
+        int buffer = AudioMaster.loadSound("res/sounds/bounce.wav");
+        Source source = new Source();
+
+        char c = ' ';
+        while (c != 'q'){
+            c = (char) System.in.read();
+
+            if (c == 'p'){
+                source.play(buffer);
+            }
+        }
+        source.delete();
+        AudioMaster.cleanUp();
+    }
+}
