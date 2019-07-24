c07a077160e2ec5b51f02cabdf9d75f2bf710ce6
Abhinav Akkena <absman09@gmail.com>
2019-07-24 10:10:54 +0100

    Can now play 3D sound effects!

--- src/audio/AudioTest.java
+++ src/audio/AudioTest.java
@@ -4,20 +4,24 @@ package audio;
 import java.io.IOException;
 
 public class AudioTest {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         AudioMaster.init();
-        AudioMaster.setListenerData();
+        AudioMaster.setListenerData(0 ,0, 0);
 
         int buffer = AudioMaster.loadSound("res/sounds/bounce.wav");
         Source source = new Source();
 
+        source.setLooping(true);
+        source.play(buffer);
+
+        float xPos = 8;
+        source.setPosition(xPos, 0, 2);
+
         char c = ' ';
         while (c != 'q'){
-            c = (char) System.in.read();
-
-            if (c == 'p'){
-                source.play(buffer);
-            }
+            xPos -= 0.03f;
+            source.setPosition(xPos, 0, 2);
+            Thread.sleep(10);
         }
         source.delete();
         AudioMaster.cleanUp();
